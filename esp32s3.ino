8560f48f2cd8b2473ffa14a575c774fe236e42ae
DRC Lab <85211068+DRCRecoveryData@users.noreply.github.com>
2024-09-08 15:51:05 +0700

    Update esp32s3.ino

--- esp32s3.ino
+++ esp32s3.ino
@@ -1,6 +1,21 @@
-#include <USBMIDI.h>
+#if ARDUINO_USB_MODE
+#warning "This sketch should be used when USB is in OTG mode"
 
-// The note numbers corresponding to the buttons in the matrix
+// This section will be used when USB is in OTG mode.
+// You can include the specific setup for OTG mode here if needed.
+void setup() {
+  // Setup code for OTG mode
+}
+
+void loop() {
+  // Loop code for OTG mode
+}
+
+#else
+
+#include "USB.h"
+#include "USBMIDI.h"
+
 const int addresses[4][4] = {
   {36, 37, 38, 39},
   {40, 41, 42, 43},
@@ -8,11 +23,9 @@ const int addresses[4][4] = {
   {48, 49, 50, 51}
 };
 
-// Pins for the rows and columns of the button matrix
 const int rowPins[4] = {4, 5, 6, 7};
 const int colPins[4] = {15, 16, 17, 18};
 
-// Variables to store the previous state of each button
 bool previousState[4][4] = {0};
 
 USBMIDI MIDI; // Use USBMIDI instance for MIDI communication
@@ -26,7 +39,6 @@ void loop() {
   checkButtonMatrix();
 }
 
-// Function to set up the button matrix
 void setupButtonMatrix() {
   for (int i = 0; i < 4; i++) {
     pinMode(rowPins[i], INPUT_PULLUP); // Set row pins as input with pull-up resistors
@@ -35,7 +47,6 @@ void setupButtonMatrix() {
   }
 }
 
-// Function to check the state of the buttons in the matrix
 void checkButtonMatrix() {
   for (int i = 0; i < 4; i++) {
     digitalWrite(colPins[i], LOW); // Set current column pin to low
@@ -54,7 +65,6 @@ void checkButtonMatrix() {
   }
 }
 
-// Function to send MIDI messages
 void sendMIDIMessage(int note, bool isNoteOn) {
   if (isNoteOn) {
     MIDI.noteOn(note, 127, 1); // Send MIDI note-on message, velocity 127, channel 1
@@ -62,3 +72,5 @@ void sendMIDIMessage(int note, bool isNoteOn) {
     MIDI.noteOff(note, 0, 1); // Send MIDI note-off message, velocity 0, channel 1
   }
 }
+
+#endif /* ARDUINO_USB_MODE */
